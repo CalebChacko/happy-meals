c660de25cb2bdaef2bb8ff6e9885a48eeb75e138
Caleb Chacko <ctchacko@uwaterloo.ca>
2022-03-05 13:45:32 -0500

    Extracted hrefs from search page and comments from each recipe

--- Scraping/foodnetwork.py
+++ Scraping/foodnetwork.py
@@ -1,31 +1,18 @@
-
+import os
 import time
 import requests
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 import re
 
-def search_recipes(browser, item):
-    url = 'https://www.foodnetwork.com/search/' + item + '-'
-    
-    # collect all recipes from page
-    search_page = requests.get(url).text
-    soup = BeautifulSoup(search_page, 'html.parser')
-    css_selector = "#mod-site-search-results-1 > section:nth-child(2)" \
-                    " > div > div > div.m-MediaBlock__m-TextWrap > h3 > a"
-    h3 = soup.select(css_selector)
-    # recipes = soup.find_all("a", attrs={'href': re.compile("^//www.foodnetwork.com/recipes/")})
-    # #mod-site-search-results-1 > section:nth-child(2) > div > div > div.m-MediaBlock__m-TextWrap > h3 > a
-    # print(recipes[0])
-    print(len(h3))
-    # print(h3)
+def extract_recipe(browser, url, first_time):
+    url = 'http:' + url
+    browser.get(url)
+    time.sleep(1)
 
-    # browser.get(url)
+    if first_time:
+        browser.find_element(By.CLASS_NAME, "o-InternationalDialog__a-Button--Text").click()
 
-    # go to first recipe
-    test = "https://www.foodnetwork.com/recipes/tyler-florence/spaghetti-alla-carbonara-recipe-1914140"
-    browser.get(test)
-    browser.find_element(By.CLASS_NAME, "o-InternationalDialog__a-Button--Text").click()
     browser.find_element(By.CLASS_NAME, "comments-sort-toggle").click()
     browser.find_element(By.XPATH, "//*[@id='mod-user/comments-feed-1']/div[2]/div/ul/li[3]").click()
     for i in range(5):
@@ -36,11 +23,64 @@ def search_recipes(browser, item):
             break
     
     recipe_html = browser.page_source
-    print(recipe_html[0:100]) 
     r_soup = BeautifulSoup(recipe_html, 'html.parser')
-    print(r_soup)
-    ingredients = r_soup.find('o-Ingredients__m-Body')
-    print(ingredients)
+
+    try:
+        name = r_soup.find('span', attrs={"class": "o-AssetTitle__a-HeadlineText"})
+        r_time = r_soup.find('span', attrs={"class": "o-RecipeInfo__a-Description m-RecipeInfo__a-Description--Total"})
+        ingredients = r_soup.find('div', attrs={"class": "o-Ingredients__m-Body"})
+        comments = r_soup.find('div', attrs={"class": "comments loaded"})
+
+        print('Name:', name)
+        print('Time:', r_time)
+        print('Ingredients:', str(ingredients.contents)[0:100])
+        print('Comments:', str(comments.contents)[0:100])
+    except:
+        with open(os.getcwd() + '/error_html.txt', 'w') as f:
+            f.write(str(recipe_html))
+            f.close()
+
+    return [name, r_time, ingredients, comments]
+
+
+def search_recipes(browser, item, num_pages):
+
+    links = []
+    num_links = 0
+    total_links = 25
+    i = 1
+
+    while num_links < total_links:
+        url = 'https://www.foodnetwork.com/search/' + item + '-/p/' + str(i+1)
+    
+        # collect all recipes from page
+        search_page = requests.get(url).text
+        soup = BeautifulSoup(search_page, 'html.parser')
+
+        for tag in soup.find_all('section', attrs={"class": "o-RecipeResult"}):
+            section_soup = BeautifulSoup(str(tag.contents))
+            hrefs = section_soup.find_all('a')
+            links.append(hrefs[0].get('href'))
+            num_links += 1
+
+        i += 1
+            
+    print('Num Recipes: ', len(links), ' Should be: ', total_links)
 
     data = []
+
+    i = 0
+    first = True
+    for link in links:
+        if i > 1:
+            break
+        elif i > 0:
+            first = False
+
+        result = extract_recipe(browser, link, first)
+        data.append(result)
+        
+        i += 1
+
+    
 
--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 from Scraping import scrape_tools as st
 from Scraping import foodnetwork as fn
 
-user_search = 'spaghetti'
+user_search = 'chicken'
 
-view_browser = False
+view_browser = True
 download_path = os.getcwd() + './Storage/'
 # browser = ''
 
 browser = st.setup_browser(view_browser, download_path)
 
-fn.search_recipes(browser, user_search)
+fn.search_recipes(browser, user_search, 5)
