2967042b0f28c03e7fc032b68b3a819b1d5c06fd
Caleb Chacko <ctchacko@uwaterloo.ca>
2022-03-15 15:47:47 -0400

    Output Ratings to CSV and txt file

--- Scraping/foodnetwork.py
+++ Scraping/foodnetwork.py
@@ -4,34 +4,59 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import re
 
+def parse_time(cook_time):
+    minutes = 0
+    
+    if 'hr' in cook_time:
+        time_split = cook_time.split('hr')
+        hour = int(time_split[0])
+        minutes += int(time_split[0])*60
+        cook_time = time_split[1]
+        
+    if 'min' in cook_time:
+        time_split = cook_time.split('min')
+        minutes += int(time_split[0])        
+    return minutes
+
 def extract_recipe(browser, url, first_time):
-    url = 'http:' + url
     browser.get(url)
     print(url)
     time.sleep(1)
 
     if first_time:
+        # Pop up based on location
         browser.find_element(By.CLASS_NAME, "o-InternationalDialog__a-Button--Text").click()
 
+    try:
+        # Confirm this page is a recipe
+        WebDriverWait(browser, 3).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "comments-sort-toggle")))
+    except:
+        print('Error parsing: ', url)
+        return []
+
     browser.find_element(By.CLASS_NAME, "comments-sort-toggle").click()
-    browser.find_element(By.XPATH, "//*[@id='mod-user/comments-feed-1']/div[2]/div/ul/li[3]").click()
-    for i in range(5):
-        try:
-            browser.find_element(By.CLASS_NAME, "comments-more").click()
-            time.sleep(0.2)
-        except:
-            break
+    # first_review = browser.find_element(By.XPATH, "//*[text()='Be the first to review']")
+    if 'Be the first to review' not in browser.page_source:
+        sort_by = browser.find_element(By.XPATH, "//*[@id='mod-user/comments-feed-1']/div[2]/div/ul/li[3]")
+        sort_by.click()
+
+        for i in range(5):
+            try:
+                browser.find_element(By.CLASS_NAME, "comments-more").click()
+                time.sleep(0.2)
+            except:
+                break
     
     recipe_html = browser.page_source
     r_soup = BeautifulSoup(recipe_html, 'html.parser')
 
-    
     name = r_soup.find('span', attrs={"class": "o-AssetTitle__a-HeadlineText"})
     cook_time = r_soup.find('span', attrs={"class": "o-RecipeInfo__a-Description m-RecipeInfo__a-Description--Total"})
-    ingredients = r_soup.find_all('p', attrs={"class": "o-Ingredients__a-Ingredient"})
-    comment_section = r_soup.find('div', attrs={"class": "comments loaded"})
 
     items = []
     i = 0
@@ -42,9 +67,6 @@ def extract_recipe(browser, url, first_time):
         items.append(str(ingr.get_text()).strip())
         i += 1
 
-    # 'div[data-asin="099655596X"]'
-    # 'div', {"data-level": "0"}
-
     comments = []
     for c in r_soup.find_all('div', {"data-level": "0"}):
         c_soup = BeautifulSoup(str(c.contents), 'html.parser')
@@ -53,14 +75,21 @@ def extract_recipe(browser, url, first_time):
             continue
         comments.append(str(comment.get_text()).strip())
 
-    return [name.get_text(), cook_time.get_text(), items, comments, url]
+    c_time = ""
+    if cook_time is not None:
+        c_time = parse_time(cook_time.get_text())
+
+    r_name = url
+    if name is not None:
+        r_name = name.get_text()
 
 
-def search_recipes(browser, item, num_pages):
+    return [r_name, c_time, items, comments, url]
 
+
+def search_recipes(browser, item, total_links):
     links = []
     num_links = 0
-    total_links = 25
     i = 1
 
     while num_links < total_links:
@@ -73,18 +102,15 @@ def search_recipes(browser, item, num_pages):
         for tag in soup.find_all('section', attrs={"class": "o-RecipeResult"}):
             section_soup = BeautifulSoup(str(tag.contents))
             hrefs = section_soup.find_all('a')
-            links.append(hrefs[0].get('href'))
+            links.append('http:' + hrefs[0].get('href'))
             num_links += 1
 
         i += 1
             
     print('Num Recipes: ', len(links), ' Should be: ', total_links)
-    # print(links)
-
-    data = []
 
     cols = ['Name', 'Time', 'Ingredients', 'Comments', 'Link']
-    df = pd.DataFrame([['ABC', '10 minutes', 'Nothing', ['check', 'check', 'check'], 'www.blah.com']], columns=cols)
+    df = pd.DataFrame([['ABC', '10 minutes', ['Nothing', 'Nothing'], ['check', 'check', 'check'], 'www.blah.com']], columns=cols)
 
     i = 0
     first = True
@@ -92,14 +118,20 @@ def search_recipes(browser, item, num_pages):
         # if i > 1:
         #     break
         
-        if i > 0:
-            first = False
-
         result = extract_recipe(browser, link, first)
+        if len(result) == 0:
+            continue
+
         recipe = pd.DataFrame([result], columns=cols)
         df = pd.concat([df, recipe], axis=0)
-        df.to_csv('./recipes.csv', index = False, header=True)
+        df.to_csv('./Data/Temp_Storage/' + item + '_recipes.csv', index = False, header=True)
         i += 1
+        
+        if first:
+            df = df.tail(df.shape[0] -1)
+            first = False
+
+    df.to_csv('./Data/Temp_Storage/' + item + '_recipes.csv', index = False, header=True)
 
     
 
--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import os
+import pandas as pd
 from Scraping import scrape_tools as st
 from Scraping import foodnetwork as fn
+import rate_recipes as rr
+from ast import literal_eval
 
-user_search = 'chicken'
+# SETUP
+with open('./Data/input.txt') as f:
+    lines = f.readlines()
+
+user_search = lines[0]
+user_search = user_search.replace(' ', '-')
 
 view_browser = False
-download_path = os.getcwd() + './Storage/'
-# browser = ''
+download_path = os.getcwd() + './Data/'
 
 browser = st.setup_browser(view_browser, download_path)
 
-fn.search_recipes(browser, user_search, 5)
+# if view_browser:
+#     debug_url = '//www.foodnetwork.com/recipes/ellie-krieger/chicken-zucchini-alfredo-recipe-2104237'
+#     fn.extract_recipe(browser, debug_url, True)
+#     exit()
+
+# EXTRACT RECIPES
+fn.search_recipes(browser, user_search, 3)
 browser.close()
+
+# RATE RECIPES
+recipes = pd.read_csv('./Data/Temp_Storage/' + user_search + '_recipes.csv')
+recipe = rr.rate_comments(recipes, user_search)
+
+# OUTPUT INGREDIENTS
+with open('./Data/Output/output.txt', 'w') as f:
+    f.write('Name: ' + recipe['Name'] + '\n')
+    f.write('Rating: ' + str(recipe['Rating']) + '\n')
+    f.write('Time: ' + str(recipe['Time']) + ' minutes\n')
+    f.write('Details: ' + str(recipe['Link']) + '\n')
+    f.write('\n')
+    f.write('Ingredients \n')
+    ingrs = literal_eval(recipe[2])
+    
+    for line in ingrs:
+        f.write(line)
+        f.write('\n')
